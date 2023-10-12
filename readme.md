5ef07b2f9012b3dbad516d67a21d1885a39213b4
jckang <jckang568@gmail.com>
2023-10-12 10:46:36 +0900

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Wanted Pre-onboarding Backend Internship Assignment
+
+
+## 1. 개요
+
+**원티드**에서 진행하는 교육형 인턴쉽 과제입니다.[(링크)](https://bow-hair-db3.notion.site/1850bca26fda4e0ca1410df270c03409)
---
+++ src/test/java/kr/co/wanted/WantedPreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
